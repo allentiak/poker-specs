00c14fa8cee6538e28347df67f390acf1097bfa3
Leandro Doctors <ldoctors@gmail.com>
2021-01-21 16:08:37 -0300

    test: 'two-pairs?'

--- test/allentiak/poker/hand_test.clj
+++ test/allentiak/poker/hand_test.clj
@@ -12,3 +12,17 @@
 (deftest is-not-one-pair-test
   (testing "not one-pair?"
     (is (not (hand/one-pair? hand-without-a-pair)))))
+
+(def hand-with-two-pairs [[3 :diamonds] [3 :spades] [2 :hearts] [2 :clubs] [4 :clubs]])
+
+;; FIXME: the card '[1 :diamonds]' should not conform...
+;; Maybe should check spec first?
+(def hand-without-two-pairs [[1 :diamonds] [2 :diamonds] [3 :diamonds] [4 :diamonds] [5 :diamonds]])
+
+(deftest is-two-pairs-test
+  (testing "two-pairs?"
+    (is (hand/two-pairs? hand-with-two-pairs))))
+
+(deftest is-not-two-pairs-test
+  (testing "not two-pairs?"
+    (is (not (hand/two-pairs? hand-without-two-pairs)))))
